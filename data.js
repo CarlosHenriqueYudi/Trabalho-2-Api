547c692d76291836d7424c1ce889547aaa6b6810
CarlosHenriqueYudi <carlosyudi@edu.unifil.br>
2026-04-05 16:45:43 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+let livros = [
+  { id: 1, titulo: "Dom Casmurro", autor: "Machado de Assis", ano: 1899 },
+  { id: 2, titulo: "1984", autor: "George Orwell", ano: 1949 },
+  { id: 3, titulo: "O Hobbit", autor: "J.R.R. Tolkien", ano: 1937 },
+  { id: 4, titulo: "A Revolução dos Bichos", autor: "George Orwell", ano: 1945 },
+  { id: 5, titulo: "Capitães da Areia", autor: "Jorge Amado", ano: 1937 },
+  { id: 6, titulo: "O Pequeno Príncipe", autor: "Saint-Exupéry", ano: 1943 },
+  { id: 7, titulo: "Harry Potter", autor: "J.K. Rowling", ano: 1997 },
+  { id: 8, titulo: "Senhor dos Anéis", autor: "Tolkien", ano: 1954 },
+  { id: 9, titulo: "It", autor: "Stephen King", ano: 1986 },
+  { id: 10, titulo: "Drácula", autor: "Bram Stoker", ano: 1897 }
+];
+
+let proximoId = 11;
+
+module.exports = { livros, proximoId };
